30a5a0e7501d995bd46459532c03c83b393fafc6
Espen Amble Kolstad <espenamblekolstad@gmail.com>
2021-12-16 17:44:34 +0100

    Day 16 solution - separate reading from evaluation

--- src/day16.clj
+++ src/day16.clj
@@ -8,13 +8,9 @@
 (def hex->bit {\0 "0000", \1 "0001", \2 "0010", \3 "0011", \4 "0100", \5 "0101", \6 "0110", \7 "0111", \8 "1000", \9 "1001",
                \A "1010", \B "1011", \C "1100", \D "1101", \E "1110", \F "1111"})
 
-(defn take-until [pred coll]
-  (transduce (halt-when pred (fn [r h] (conj r h))) conj [] coll))
-
 (defn bits->n [bs] (Long/parseLong (str/join bs) 2))
 
-(defn parse [input]
-  (mapcat hex->bit input))
+(defn parse [input] (mapcat hex->bit input))
 
 (defn ->header [bs]
   (when (> (count bs) 6)
@@ -22,50 +18,48 @@
           [t msg] (split-at 3 msg)]
       [(bits->n v) (bits->n t) msg])))
 
+(defn gt [a b] (if (> a b) 1 0))
+(defn lt [a b] (if (< a b) 1 0))
+(defn eq [a b] (if (= a b) 1 0))
+
+(def t->op {0 '+, 1 '*, 2 'min, 3 'max, 5 'gt, 6 'lt, 7 'eq})
+
+(defn read-literal [bs]
+  (let [[nbs [t]] (split-with (comp #{\1} first) (partition 5 bs))]
+    [(bits->n (mapcat next (concat nbs [t]))) (drop (* 5 (inc (count nbs))) bs)]))
+
+(defn read-op [rdr op bs]
+  (let [[is rest] (if (= \0 (first bs))
+                    (let [[bs more] (split-at 15 (next bs))
+                          len (bits->n bs)]
+                      [(first (rdr (take len more) len)) (drop len more)])
+                    (let [[bs more] (split-at 11 (next bs))]
+                      (rdr more (bits->n bs))))]
+    [(into [op] is) rest]))
+
+(defn read-pks [bs n]
+  (loop [bs bs is [] n n]
+    (if-let [[v t bs] (and (pos? n) (->header bs))]
+      (let [[i bs] (if (= 4 t)
+                     (read-literal bs)
+                     (read-op read-pks (t->op t) bs))]
+        (recur bs (conj is (conj [v] i)) (dec n)))
+      [is bs])))
+
+(defn unpack-v [[v e]] (if (coll? e) (apply + v (map unpack-v (rest e))) v))
+
 (defn part-1 [input]
-  (loop [msg (parse input) vs []]
-    (if-let [[v t msg] (->header msg)]
-      (let [[v msg] (if (= 4 t)
-                      [v (drop (* 5 (count (take-until (comp #{\0} first) (partition 5 msg)))) msg)]
-                      (if (= \0 (first msg))
-                        [v (drop 15 (next msg))]
-                        [v (drop 11 (next msg))]))]
-        (recur msg (conj vs v)))
-      (reduce + vs))))
+  (-> (parse input) (read-pks 1) ffirst unpack-v))
 
 (comment
 
  (part-1 input)                                             ;; 913
  )
 
-(defn gt [a b] (if (> a b) 1 0))
-(defn lt [a b] (if (< a b) 1 0))
-(defn eq [a b] (if (= a b) 1 0))
-
-(def t->op {0 +, 1 *, 2 min, 3 max, 5 gt, 6 lt, 7 eq})
-
-(defn read-pks [bs n]
-  (loop [bs bs is [] n n]
-    (if (and (pos? n) (next bs))
-      (let [[_ t bs] (->header bs)
-            op (t->op t)
-            [i bs] (if (= 4 t)
-                     (let [nbs (take-until (comp #{\0} first) (partition 5 bs))]
-                       [(bits->n (mapcat next nbs)) (drop (* 5 (count nbs)) bs)])
-                     (if (= \0 (first bs))
-                       (let [[bs more] (split-at 15 (next bs))
-                             len (bits->n bs)
-                             [is] (read-pks (take len more) len)]
-                         [(apply op is) (drop len more)])
-                       (let [[bs more] (split-at 11 (next bs))
-                             cnt (bits->n bs)
-                             [i bs] (read-pks more cnt)]
-                         [(apply op i) bs])))]
-        (recur bs (conj is i) (dec n)))
-      [is bs])))
+(defn unpack-op [[_ e]] (if (coll? e) (cons (first e) (map unpack-op (rest e))) e))
 
 (defn part-2 [input]
-  (ffirst (read-pks (parse input) 1)))
+  (-> (parse input) (read-pks 1) ffirst unpack-op eval))
 
 (comment
 
