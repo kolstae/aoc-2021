676d7cb213a8ab12e7566b15e34b899f0e9cb35f
Espen Amble Kolstad <espenamblekolstad@gmail.com>
2021-12-23 16:40:40 +0100

    Day 23 solution - mostly stolen :(

--- src/day23.clj
+++ src/day23.clj
@@ -10,92 +10,65 @@
   (mapv vec (butlast (next (str/split-lines input)))))
 
 (def cost {\A 1 \B 10 \C 100 \D 1000})
-(def dest-x {\A 3 \B 5 \C 7 \D 9})
-(def no-stop-x (into #{} (vals dest-x)))
+(def bot-idx {\A 2 \B 4 \C 6 \D 8})
 
-(defn debug [ls]
-  (let [ls (if (map? ls)
-             (for [y (range 5)] (for [x (range 13)] (get ls [y x] \#)))
-             ls)]
-    (doseq [l ls]
-      (println (str/join l))))
-  ls)
+(defn done? [bs] (every? (fn [[k vs]] (apply = k vs)) bs))
+(defn move-from? [b bs] (some (fn [v] (and v (not= b v))) bs))
+(defn move-to? [b bs] (every? (fn [v] (or (not v) (= b v))) bs))
 
-(defn distance [[x1 y1] [x2 y2]] (+ (Math/abs ^long (- x1 x2)) (Math/abs ^long (- y1 y2))))
-(defn can-move? [m [y x]] (some (comp #{\.} (partial get m)) [[(dec y) x] [y (inc x)] [y (dec x)]]))
+(defn clear-path? [bi ti hw]
+  (every? (fn [i] (nil? (bot-idx (get hw i))))
+          (if (< bi ti)
+            (range (inc bi) ti)
+            (range (inc ti) bi))))
 
-(defn part-1 [input]
-  )
+(defn solve [lines]
+  (let [s (into {} (mapcat
+                    (fn [y l] (keep-indexed (fn [x c] (when (#{\. \A \B \C \D} c) [[y x] c])) l))
+                    (range) lines))
+        y-to (inc (apply max (map first (keys s))))
+        start [(into {} (map (juxt identity (fn [c] (mapv #(get s [% (inc (bot-idx c))]) (range 1 y-to))))) (keys cost))
+               (reduce (fn [v i] (assoc v i :x)) (vec (repeat 11 nil)) (vals bot-idx))]
 
-(comment
-
- (let [lines (parse small-input)
-       ;[h t] (split-at 2 lines)
-       ;lines (concat h (map vec (str/split-lines "  #D#C#B#A#\n  #D#B#A#C#")) t)
-       s (debug (into {} (mapcat
-                          (fn [y l] (keep-indexed (fn [x c] (when (#{\. \A \B \C \D} c) [[y x] c])) l))
-                          (range) lines)))
-       y-to (inc (apply max (map first (keys s))))
-       ->>debug (fn [s] (prn s) s)
-
-       home? (fn [m y x c]
-               (when-let [dx (dest-x c)]
-                 (and (= x dx)
-                      (every? #{c} (map #(get m [% x]) (range (inc y) y-to))))))
-
-       ->dxr (fn [x dx] (if (< x dx) (range (inc x) dx) (range (dec x) dx -1)))
-
-       possible-moves (fn [m [y x c :as a]]
-                        (if (= y 0)
-                          (let [dx (dest-x c)
-                                pp (when (every? #(= \. (get m [y %])) (->dxr x dx))
-                                     (keep #(when-let [dc (#{c \.} (get m [% dx]))] [% dx dc]) (range 1 y-to)))]
-                            (when (next pp) (map #(vector [y x c] (subvec % 0 2)) (take-last 1 (filter (comp #{\.} last) pp)))))
-                          (map #(vector a [0 %])
-                               (concat (sequence (comp (take-while #(= \. (get m [0 %]))) (remove no-stop-x)) (range (inc x) 12))
-                                       (take-while #(= \. (get m [0 %])) (remove no-stop-x (range (dec x) 1 -1)))))))
-       all-possible-moves (fn [m] (->> m
-                                       (sequence (comp
-                                                  (keep (fn [[[y x] c]]
-                                                          (when (and (dest-x c) (not (home? m y x c)))
-                                                            [y x c])))
-                                                  (filter (partial can-move? m))
-                                                  (mapcat (partial possible-moves m))))
-                                       (sort-by (fn [[a b]] (distance a b)))
-                                       seq))
-       move (fn [m [y1 x1 c] d] [(* (cost c) (distance [y1 x1] d)) (-> (assoc m d c) (assoc [y1 x1] \.))])
-       solve-r (fn [s]
-                 (loop [s s ms (all-possible-moves s) e 0
-                        q '() max-e Long/MAX_VALUE]
-
-                   (if-let [m (first ms)]
-                     (let [[d s'] (apply move s m)
-                           #_(debug s)
-                           used-e (+ e d)]
-                       (if (>= used-e max-e)
-                         (recur {} [] max-e q max-e)
-                         (recur s' (all-possible-moves s') used-e (cond-> q (next ms) (conj [s (rest ms) e])) max-e)))
-                     (let [max-e (if (->> s
-                                          (map (partial apply conj))
-                                          (filter (comp cost last))
-                                          (every? (partial apply home? s)))
-                                   e
-                                   max-e)]
-                       (if-let [[s ms e] (peek q)]
-                         (do #_(prn :q max-e ms) (recur s ms e (pop q) max-e))
-                         max-e)))))]
-   #_(home? s 2 7 \C)
-   (time (solve-r s)))
-
- (peek (list 5))
-
- (time (part-1 input))                                      ;; 15365
- )
-
-(defn part-2 [input]
-  )
+        cache (atom {})
+        solver (fn this [[bm hw :as s]]
+                 (if (done? bm)
+                   0
+                   (if-let [cv (@cache s)]
+                     cv
+                     (if-let [[cost s'] (first
+                                         (for [[i b] (map-indexed vector hw)
+                                               :when (char? b)
+                                               :let [bs (bm b)]
+                                               :when (move-to? b bs)
+                                               :let [bi (bot-idx b)]
+                                               :when (clear-path? bi i hw)]
+                                           (let [[di] (last (remove second (map-indexed vector bs)))]
+                                             [(* (cost b) (+ 1 di (Math/abs ^long (- bi i))))
+                                              [(assoc-in bm [b di] b) (assoc hw i nil)]])))]
+                       (+ cost (this s'))
+                       (let [cost (reduce
+                                   min
+                                   Integer/MAX_VALUE
+                                   (for [[b bs] bm
+                                         :when (move-from? b bs)
+                                         :let [bi (bot-idx b)
+                                               [ki c] (first (filter second (map-indexed vector bs)))]
+                                         [ti] (remove second (map-indexed vector hw))
+                                         :when (clear-path? bi ti hw)]
+                                     (let [cost (* (cost c) (+ 1 ki (Math/abs ^long (- ti bi))))]
+                                       (+ cost (this [(assoc-in bm [b ki] nil) (assoc hw ti c)])))))]
+                         (swap! cache assoc s cost)
+                         cost)))))]
+    (time (solver start))))
 
 (comment
 
- (time (part-2 input))                                      ;;
+ (solve (parse input))                                      ;; 15365
+
+ ;; Had to steal almost everything from https://github.com/jonathanpaulson/AdventOfCode/blob/master/2021/23.py :(
+ (let [lines (parse input)
+       [h t] (split-at 2 lines)
+       lines (concat h (map vec (str/split-lines "  #D#C#B#A#\n  #D#B#A#C#")) t)]
+   (solve lines))                                           ;; 52055
  )
